bc9161b42655ef22af9371460189a253ea2bc1ba
josuemora <josue.mora@omnilife.com>
2014-05-22 11:09:28 -0500

    Initial Commit

---
+++ tableview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tableview
+//
+//  Created by josue mora on 22/05/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tableview/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tableview
+//
+//  Created by josue mora on 22/05/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
